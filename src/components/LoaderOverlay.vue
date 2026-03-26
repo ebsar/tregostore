0ef58686014dea033a6d4e0699b13c2d6cb9606a
ebsar <ebsar.hoxha@gmail.com>
2026-03-26 23:51:37 +0100

    p

--- src/components/LoaderOverlay.vue
+++ src/components/LoaderOverlay.vue
@@ -5,26 +5,13 @@
     <div class="app-loader-glow app-loader-glow-right"></div>
     <div class="app-loader-panel">
       <div class="app-loader-stage">
-        <div class="app-loader-orbit app-loader-orbit-outer"></div>
-        <div class="app-loader-orbit app-loader-orbit-inner"></div>
-        <div class="app-loader-orbit app-loader-orbit-core"></div>
-
         <div class="app-loader-centerpiece">
           <img class="app-loader-logo" src="/trego-logo.png" alt="TREGO">
-          <span class="app-loader-center-shine"></span>
         </div>
       </div>
 
       <div class="app-loader-copy-block">
-        <strong class="app-loader-brand">tregostore</strong>
-        <p class="app-loader-copy">
-          duke u ngarkuar
-          <span class="app-loader-dots" aria-hidden="true">
-            <span></span>
-            <span></span>
-            <span></span>
-          </span>
-        </p>
+        <p class="app-loader-copy">duke u ngarkuar</p>
       </div>
     </div>
   </div>
