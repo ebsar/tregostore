0c93c6b65f95f9d0ae8d944e1de1cf78de7e77c3
ebsar <ebsar.hoxha@gmail.com>
2026-03-27 05:23:45 +0100

    p

--- src/components/LoaderOverlay.vue
+++ src/components/LoaderOverlay.vue
@@ -2,33 +2,13 @@
   <div class="app-loader" aria-hidden="true">
     <div class="app-loader-backdrop"></div>
     <div class="app-loader-panel">
-      <div class="app-loader-icons">
-        <div class="app-loader-icon-chip app-loader-icon-chip-bag">
-          <svg class="app-loader-icon" viewBox="0 0 24 24" aria-hidden="true">
-            <path d="M7 8h10l1 11H6z"></path>
-            <path d="M9 9V7a3 3 0 0 1 6 0v2"></path>
-          </svg>
-        </div>
-
-        <div class="app-loader-icon-chip app-loader-icon-chip-cart">
-          <svg class="app-loader-icon" viewBox="0 0 24 24" aria-hidden="true">
-            <path d="M3 5h2l2.1 9.1a1 1 0 0 0 1 .8h8.8a1 1 0 0 0 1-.8L20 8H7.2"></path>
-            <circle cx="10" cy="19" r="1.4"></circle>
-            <circle cx="18" cy="19" r="1.4"></circle>
-          </svg>
-        </div>
-
-        <div class="app-loader-icon-chip app-loader-icon-chip-box">
-          <svg class="app-loader-icon" viewBox="0 0 24 24" aria-hidden="true">
-            <path d="M12 3 4.5 7v10L12 21l7.5-4V7z"></path>
-            <path d="M12 21V11"></path>
-            <path d="M4.5 7 12 11l7.5-4"></path>
-          </svg>
-        </div>
+      <div class="app-loader-stage">
+        <img class="app-loader-logo" src="/trego-logo.png" alt="TREGO" width="420" height="159" fetchpriority="high">
       </div>
 
-      <strong class="app-loader-brand">Tregostore</strong>
-      <p class="app-loader-copy">duke u ngarkuar</p>
+      <p class="app-loader-copy">
+        Po shkarkohet<span class="app-loader-dots" aria-hidden="true"><span>.</span><span>.</span><span>.</span></span>
+      </p>
     </div>
   </div>
 </template>
